7b5164910ee1fb26d971dfb95fa3884ab341cc92
mariiono12 <bustosian432@gmail.com>
2026-03-12 11:13:18 -0300

    actualización

--- 03_bucle_for/static/js/script.js
+++ 03_bucle_for/static/js/script.js
@@ -1,5 +1,5 @@
 console.log("conectando");
-//*
+//*El Contador en Pantalla
 function contadorPantalla() {
     let numeros = [];
     const container = document.getElementById('resultadoContainer');
@@ -11,8 +11,34 @@ function contadorPantalla() {
     container.classList.remove('d-none');
 };
 
-//*
-function  Lista de Asistencia Automática() {
+//*Lista de Asistencia Automática
+function asistenciaAutomatica() {
     let curso = ["Ana", "Diego", "Sofía", "Matias"];
-
+const container = document.getElementById(`container2`)
+const resultado = document.getElementById(`resultado2`)
+resultado.textContent= ""; // Limpiar contenedor
+for(let i = 0; i < curso.length; i++) {
+    resultado.textContent += `${curso[i]}, `
+}
+container.classList.remove(`d-none`)
 };
+
+//*Buscador de Aprobados (Escala 1 a 7)
+function buscadorApoderados(){
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0]
+    let contadorAprobados = 0
+const container = document.getElementById(`container3`); //selección elementos html
+const resultado = document.getElementById(`resultado3`);
+for (let i = 0; i >= notas.length; i++) {
+    if (nota[1] >= 4.0) {
+        contadorAprobados++;
+
+    }
+ 
+} 
+ console.log(resultado, contadorAprobados)
+ resultado.textoContent = "Total de alumnos aprobados: "+ contadorAprobados
+ container.classListremove(`d-none`)
+
+}
+;
