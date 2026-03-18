bd64784141595331126c7a387bf2cfa288e0e08a
mariiono12 <bustosian432@gmail.com>
2026-03-18 15:11:28 -0300

    actualizaci

--- 04_funciones_js/static/js/script.js
+++ 04_funciones_js/static/js/script.js
@@ -1,7 +1,42 @@
 console.log("conectando");
 
 let asistencia = [];
-function registrarAlumno() {
- nombreAlumno = document.getElementById("input-nombre");
-    
+function agregarLista(nombre) {
+    asistencia.push(nombre);
+    return asistencia.join(", ");
+
 }
+function registrarAlumno() {
+
+    const container = document.getElementById("container1");
+    const result = document.getElementById("resultado1");
+    const input = document.getElementById("input1");
+    let nombre = input.value;
+    // input = parseInt(input); ---> Transformar a número
+    let resultado = agregarLista(nombre); //Llamado a la función con envío de parámetros
+    result.textContent = resultado;
+    input.value = "";
+    container.classList.remove("d-none");
+}
+
+//* 
+let pacientes = ["Carlos", "María", "Diego"];
+function agregarUrgencia(nombre) {
+    pacientes.unshift(nombre);
+    return pacientes.join(", ");
+};
+
+function ingresarUrgencia() {
+    const container = document.getElementById("resultadoContainer2");
+    const result = document.getElementById("resultado2");
+    const input = document.getElementById("input2");
+    if (input.value !== "") {
+        let nombre = input.value;
+        let resultado = agregarUrgencia(nombre);
+        result.textContent = resultado;
+        input.value = "";
+        container.classList.remove("d-none");
+    } else {
+        alert("El nombre no puede estar vacío . . .");
+    }
+};
